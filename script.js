32f61188938a75e5a566825ca559845f8d933a18
GustavoLS <106133669+GustavoLS1@users.noreply.github.com>
2024-07-23 19:56:05 -0500

    Initial: First commit

---
+++ script.js
@@ -0,0 +1,13 @@
+let pass_field = document.querySelector('.pass-key');
+let show_btn = document.querySelector('.show')
+
+show_btn.addEventListener('click', function (){
+    if(pass_field.type === 'password'){
+        pass_field.type = "text";
+        show_btn.innerHTML = "<em class='bx bx-hide'></em>"
+    } else {
+        pass_field.type = 'password';
+        // show_btn.innerHTML = "<em class='bx show-alt'></em>"
+    }
+});
+
